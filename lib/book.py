cccb2c84a635cdf0ee9116a168d1c818b2a5ef41
+ <+>
2023-09-18 12:49:32 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -19,11 +19,3 @@ class Book:
     
     page_count = property(get_page_count, set_page_count)
 
-# Book in book.py gets initialized with a title.
-# Book in book.py has the title passed into __init__.
-# Book in book.py can be assigned an author name.
-# Book in book.py can be assigned a page count property.
-# Book in book.py prints "page_count must be an integer" if page_count is not an integer.
-# Book in book.py can be assigned a genre.
-# Book in book.py outputs "Flipping the page...wow, you read fast!" when method turn_page() is 
-        
--- lib/shoe.py
+++ lib/shoe.py
@@ -24,12 +24,3 @@ class Shoe:
         print("Your shoe is as good as new!")
         self.condition = "New"
 
-# Shoe in shoe.py gets initialized with a brand.
-# Shoe in shoe.py has the brand passed to __init__.
-# Shoe in shoe.py can be assigned a color.
-# Shoe in shoe.py can be assigned a size.
-# Shoe in shoe.py prints "size must be an integer" if size is not an integer.
-# Shoe in shoe.py can be assigned a material.
-# Shoe in shoe.py can be assigned a condition.
-# Shoe in shoe.py says that the shoe has been repaired.
-# Shoe in shoe.py has "New" condition after repair.
